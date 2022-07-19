7c822e687e4f78e23c9aeb51691c4fbf3fb32fcf
ngpbrasil <ngpbrasil@gmail.com>
2022-07-19 14:51:34 -0300

    fix: issues

--- src/main/java/br/com/geniustest/api/entity/Alternative.java
+++ src/main/java/br/com/geniustest/api/entity/Alternative.java
@@ -12,55 +12,48 @@ import javax.persistence.Table;
 public class Alternative {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String description;
     private boolean correct;
 
     private Question question;
 
-
     public Alternative() {
     }
 
-    public Alternative(Long id)
-    {
+    public Alternative(Long id) {
         super();
         this.id = id;
     }
 
-    public Long getId(){
+    public Long getId() {
         return id;
     }
 
 
-    public String getDescription()
-    {
+    public String getDescription() {
         return this.description;
     }
 
-    public void setDescription(String description)
-    {
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    public boolean getCorrect(boolean correct)
-    {
+    public boolean getCorrect(boolean correct) {
         return this.correct;
     }
 
-    public void setCorrect(boolean correct)
-    {
+    public void setCorrect(boolean correct) {
         this.correct = correct;
 
     }
 
-    public Question getQuestion(Question question)
-    {
+    public Question getQuestion(Question question) {
         return this.question;
     }
 
-    public void setQuestion(Question question)
-    {
+    public void setQuestion(Question question) {
         this.question = question;
 
 
