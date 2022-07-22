0969ff6875b6c2a4fa23b18030780a1c0c073ee8
Anderson Chagas <anderson.almeida@izap.com.br>
2022-07-21 23:44:18 -0300

    add to entity Card GT-13

--- src/main/java/br/com/geniustest/api/entity/Candidate.java
+++ src/main/java/br/com/geniustest/api/entity/Candidate.java
@@ -1,14 +1,44 @@
 package br.com.geniustest.api.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 
+@Entity
+@Table(name = "cadidate")
 public class Candidate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    private String fullname;
+    private String email;
+
+    public Long getId()
+    {
+        return this.id;
+    }
+
+    public String getFullname()
+    {
+        return this.fullname;
+    }
+
+    public void setFullname(String fullname)
+    {
+        this.fullname = fullname;
+
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+
+    }
 }
